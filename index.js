d23c4b122cf1797a153e1b865383a2740bc0106b
Alexandr <vashekinaleksandar@yandex.ru>
2020-11-27 15:29:57 +0300

    fix: Иправление ошибок после первой проверки

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const popupNode = document.querySelector('.popup');
-const popupFormNode = document.querySelector('.popup__form');
-const popupInputNameNode = document.querySelector('.popup__input-name');
-const popupInputJobNode = document.querySelector('.popup__input-job');
-const popupCloseButtonNode = document.querySelector('.popup__close-button');
-const profileEditButtonNode = document.querySelector('.profile__edit-button');
+let popupNode = document.querySelector('.popup');
+let popupFormNode = document.querySelector('.popup__form');
+let popupInputNameNode = document.querySelector('.popup__input_type_name');
+let popupInputJobNode = document.querySelector('.popup__input_type_job');
+let popupCloseButtonNode = document.querySelector('.popup__close-button');
+let profileEditButtonNode = document.querySelector('.profile__edit-button');
 
 
-const profileAuthorNode = document.querySelector('.profile__author');
-const profileCaptionNode = document.querySelector('.profile__caption');
+let profileAuthorNode = document.querySelector('.profile__author');
+let profileCaptionNode = document.querySelector('.profile__caption');
 
-profileEditButtonNode.addEventListener('click',handleAddPopupVisibility);
-popupCloseButtonNode.addEventListener('click',handleClearForm);
-popupFormNode.addEventListener('submit', handleFormSubmit);
 
 function handleAddPopupVisibility(){
   popupNode.classList.add('popup_visible');
+  popupInputNameNode.value = profileAuthorNode.textContent;
+  popupInputJobNode.value = profileCaptionNode.textContent;
  }
 
 function handleFormSubmit(event){
   event.preventDefault();
   profileAuthorNode.textContent = popupInputNameNode.value;
   profileCaptionNode.textContent = popupInputJobNode.value;
-  popupNode.classList.remove('popup_visible');
+  handleClearForm();
 }
 
 function handleClearForm(){
   popupNode.classList.remove('popup_visible');
-  popupInputNameNode.value = profileAuthorNode.textContent;
-  popupInputJobNode.value = profileCaptionNode.textContent;
+
 }
 
-const elementsButtonLikeNode = document.querySelector('.elements__button-like');
-const elementsUnorderedListNode = document.querySelector('.elements__unordered-list');
-
-elementsUnorderedListNode.addEventListener('click', handleElementsButtonLikeActive);
-
-function handleElementsButtonLikeActive(event){
-  if (event.target.classList.contains('elements__button-like')) {
-    event.target.classList.toggle("elements__button-like_active");
-  }
-}
+profileEditButtonNode.addEventListener('click',handleAddPopupVisibility);
+popupCloseButtonNode.addEventListener('click',handleClearForm);
+popupFormNode.addEventListener('submit', handleFormSubmit);
