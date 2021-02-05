703277e5d18e0f876d1204227490b689b6eaf540
Alexandr <vashekinaleksandar@yandex.ru>
2021-02-05 19:47:40 +0300

    fix: Bug fixes after the first check

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,23 +1,22 @@
-import {
-  esc
- } from './constants.js';
-
 export default class Popup {
-  constructor(popupSelector){
+  constructor(popupSelector, esc){
     this._popupSelector = popupSelector;
     this._popup = document.querySelector(this._popupSelector);
     this._popupCloseButton = this._popup.querySelector('.popup__close-button');
+    this.esc = esc;
   }
   open(){
     this._popup.classList.add('popup_visible');
     document.addEventListener('keydown',  this._handleEscClose);
+    this._popup.addEventListener('click', this._handleClosePopupByOverlay);
   }
   close(){
     this._popup.classList.remove('popup_visible');
     document.removeEventListener('keydown', this._handleEscClose);
+    this._popup.removeEventListener('click', this._handleClosePopupByOverlay);
   }
   _handleEscClose = (event) => {
-    if (event.key === esc) {
+    if (event.key === this.esc) {
       this.close();
     }
   }
@@ -28,6 +27,5 @@ export default class Popup {
   }
   setEventListeners(){
     this._popupCloseButton.addEventListener('click', () => { this.close() });
-    this._popup.addEventListener('click', this._handleClosePopupByOverlay);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
-  entry: { main: './src/scripts/index.js' },
+  entry: { main: './src/page/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -38,8 +38,14 @@ module.exports = {
     ]
     },
     {
-    test: /\.(svg|png|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-    type: 'asset/resource'
+      test: /\.(png|svg|jpg|jpeg|gif)$/, type: 'asset/resource', generator: {
+        filename: 'images/[name].[contenthash][ext]',
+      }
+    }, 
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', generator: {
+        filename: 'vendor/[name].[contenthash][ext]',
+      }
     },
     {
       test: /\.js$/,
